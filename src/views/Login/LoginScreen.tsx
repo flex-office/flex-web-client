17aa419d12014ee8c1ab67e11e655136986bd82f
Jan ATAC <can.atac@gmail.com>
2019-10-28 20:04:40 +0100

    add version 0.3.5 label

--- src/views/Login/LoginScreen.tsx
+++ src/views/Login/LoginScreen.tsx
@@ -449,7 +449,7 @@ class LoginScreen extends React.Component<LoginScreenProps, LoginScreenState> {
               marginTop: "5rem"
             }}
           >
-            Version O.3.4
+            Version O.3.5
           </div>
         </div>
       </div>
